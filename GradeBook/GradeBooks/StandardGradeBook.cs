e959b9ac83b8468ea1b44ee1476e03854177e8ed
Jordon Trujillo <jordon.trujillo@kiewit.com>
2020-08-14 15:36:46 -0500

    Create StandardGradeBook class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        StandardGradeBook(string name) : base(name)
+        {
+        }
+    }
+}
