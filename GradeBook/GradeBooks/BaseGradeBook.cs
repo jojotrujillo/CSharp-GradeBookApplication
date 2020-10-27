fd08515841cbb98ca8d0c4f2b19a7ee170a2d207
Jordon Trujillo <jordon.trujillo@kiewit.com>
2020-10-27 13:07:15 -0500

    Update BaseGradeBook's GetGPA Method

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -109,20 +109,31 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int grade = 0;
+            
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    grade = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    grade = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    grade = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    grade = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    grade = 0;
+                    break;
             }
-            return 0;
+
+            if ((studentType == StudentType.Honors || studentType == StudentType.DualEnrolled) && IsWeighted)
+                grade += 1;
+
+            return grade;
         }
 
         public virtual void CalculateStatistics()
